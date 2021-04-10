5f6633b6e5c4a9ee9282703f42f2d7147e261334
Tautvydas <banys.tautvydas@gmail.com>
2021-04-10 17:01:04 +0300

    validate empty and add cancel event

--- utils/ask.js
+++ utils/ask.js
@@ -1,6 +1,7 @@
 const { Input } = require('enquirer');
 const to = require('await-to-js').default;
 const handleError = require(`cli-handle-error`);
+const shoudCancel = require('cli-should-cancel');
 
 module.exports = async ({ message, hint, initial }) => {
   
@@ -8,8 +9,13 @@ module.exports = async ({ message, hint, initial }) => {
     new Input({
       message,
       hint,
-      initial
-    }).run()
+      initial,
+      validate(value) {
+        return !value ? `PLease add a value` : true;
+      }
+    })
+    .on(`cancel`, () => shoudCancel())
+    .run()
   );
 
   handleError(`INPUT`, err);
